07770fa4c4614a7643918b769584dde9b51dfdf6
EstefaMH <estefymoncaleano@gmail.com>
2024-04-03 00:15:51 -0500

    add model

--- src/models/product.js
+++ src/models/product.js
@@ -1,11 +1,11 @@
 export class Product {
-    constructor(id,ref, image, name, price_in,price_out,size,features,brand,category) {
+    constructor(id,ref, name, price_in,price_out,size,features,brand,category) {
         this.id = id;
         this.ref = ref
         this.price_in = price_in;
         this.price_out = price_out;
         this.size =size ;
-        this.image = image;
+        //this.image = image;
         this.name = name;
         this.features =features;
         this.brand =brand;
