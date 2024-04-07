7263bc132636b43fe9266a1c8226d870133e5f41
EstefaMH <estefymoncaleano@gmail.com>
2024-04-06 19:12:00 -0500

    add counter

--- src/components/shoppingCartButton/ShoppingCartButton.jsx
+++ src/components/shoppingCartButton/ShoppingCartButton.jsx
@@ -1,15 +1,18 @@
-import { Fab } from "@mui/material"
+import { Fab, IconButton } from "@mui/material"
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { pathRoutes } from "../../routes/PathRoutes";
 import { Link } from "react-router-dom";
 import "./ShoppingCartButton.css"
+import Badge from '@mui/material/Badge';
 
 function ShoppingCartButton() {
     return (
         <Link className="shopping-cart-button" to={pathRoutes.shoppingCart}>
-            <Fab color="primary" aria-label="add">
-                <ShoppingCartIcon />
-            </Fab>
+            <IconButton aria-label="cart">
+                <Badge badgeContent={1} color="primary">
+                    <ShoppingCartIcon color="action" />
+                </Badge>
+            </IconButton>
         </Link>
     )
 }
